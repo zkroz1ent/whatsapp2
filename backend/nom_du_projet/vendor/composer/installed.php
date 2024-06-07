b5de5a9b37217205a81c6c0beca514f6e2d187d6
Zkroz1ent <damien.dutertre@limayrac.fr>
2024-06-07 13:53:20 +0200

    rajout des cors policy

--- backend/nom_du_projet/vendor/composer/installed.php
+++ backend/nom_du_projet/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '2a069688983ea6c773cd4fc55b3b9da7a6f4c203',
+        'reference' => 'e472369769ee1cd3a3ecf5f9075971e2f22a03a0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '2a069688983ea6c773cd4fc55b3b9da7a6f4c203',
+            'reference' => 'e472369769ee1cd3a3ecf5f9075971e2f22a03a0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
