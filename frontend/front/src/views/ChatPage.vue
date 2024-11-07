8cbaf22d4a628a51565c7137613e1f8d54b48939
YohanDavion <yohandavion31@gmail.com>
2024-11-07 15:03:25 +0100

    fix(Front): Correction d'un conflit de code

--- frontend/front/src/views/ChatPage.vue
+++ frontend/front/src/views/ChatPage.vue
@@ -4,7 +4,8 @@
     <!-- Liste des conversations -->
     <div :class="['w-1/4 bg-gray-100 dark:bg-slate-900 border-r border-gray-300 transition-transform transform', { '-translate-x-full': !isSidebarOpen }]">
       <div class="p-4 border-b border-gray-300 flex justify-between items-center">
-        <input v-model="searchQuery" type="text" placeholder="Rechercher..." class="w-full p-2 border rounded" />
+        <input v-model="searchQuery" type="text" placeholder="Rechercher..." class="p-2 border rounded" />
+        <button @click="displayModal" class="ml-2 bg-blue-500 text-white hover:bg-blue-600 p-2 rounded">Créer un Groupe</button>
         <button @click="toggleSidebar" class="ml-2 bg-green-500 text-white p-2 rounded">⇦</button>
       </div>
       <div class="overflow-y-auto h-full">
@@ -15,6 +16,14 @@
           <ul>
             <li v-for="notif in receivedNotifications" :key="notif.id">
               {{ notif.content }} - {{ notif.createdAt }}
+            </li></ul>
+          <h2 class="text-xl dark:text-white font-bold mb-2">Groupes</h2>
+          <ul>
+            <li v-for="(conversation, index) in filteredGroupConversations" :key="index"
+              @click="selectConversation(index, 'group')"
+              class="cursor-pointer dark:text-white p-4 border-b border-gray-300 hover:scale-110">
+              <div class="font-bold">{{ conversation.name }}</div>
+              <div class="text-sm dark:text-white text-gray-600">{{ conversation.lastMessage }}</div>
             </li>
           </ul>
           <button @click="showNotificationSettings = true" class="my-2 bg-blue-500 text-white p-2 rounded">Gérer les Notifications</button>
@@ -24,7 +33,57 @@
     </div>
 
     <!-- Fenêtre de chat -->
-    <div class="flex-1 flex flex-col" v-if="selectedConversation || isGlobal">
+    <div class="flex-1 flex flex-col">
+
+      
+    <div v-if="showModal" class="modal-overlay bg-gray-200">
+    <div class="modal-popup">
+      <button class="p-2 close-button bg-red-500 rounded" @click="showModal = false">&times;</button>
+    <div class="group-name">
+      <label for="groupName">Nom du Groupe :</label>
+      <input type="text" class="ml-2 rounded" id="groupName" v-model="groupName">
+    </div>
+    <div class="group-container">
+    <button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" data-dropdown-placement="bottom" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown search
+      <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+      </svg>
+    </button>
+
+    <div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
+      <div class="p-3">
+        <label for="input-group-search" class="sr-only">Search</label>
+        <div class="relative">
+          <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+          </div>
+          <input type="text" id="input-group-search" v-model="searchQuery" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search user">
+        </div>
+      </div>
+
+      <ul v-show="dropdownOpen" class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">
+        <li v-for="user in filteredUsers" :key="user.id">
+          <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+            <input :id="'checkbox-item-' + user.id" type="checkbox" v-model="selectedUsers" :value="user.id" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+            <label :for="'checkbox-item-' + user.id" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">{{ user.name }}</label>
+          </div>
+        </li>
+      </ul>
+      <a href="#" class="flex items-center p-3 text-sm font-medium text-red-600 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-red-500 hover:underline">
+        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+          <path d="M6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Zm11-3h-6a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2Z"/>
+        </svg>
+        Delete user
+      </a>
+    </div>
+  </div>
+    
+      <button @click="createUser">Créer</button>
+    </div>
+  </div>
+
       <div class="p-2 border-b border-gray-300 flex items-center">
         <button @click="toggleSidebar" class="mr-2 bg-green-500 text-white p-2 rounded">⇨</button>
         <div class="font-bold dark:text-white">{{ isGlobal ? 'Fil Global' : selectedConversation.name }}</div>
@@ -81,8 +140,85 @@ export default {
       isGlobal: false,
       selectedConversation: {},
       receivedNotifications: [],
-      showNotificationSettings: false  // Pour afficher NotificationSettings
+      showNotificationSettings: false,  // Pour afficher NotificationSettings
+      showModal: false,
+      users: [
+        { id: 1, name: 'Alice' },
+        { id: 2, name: 'Bob' },
+        { id: 3, name: 'Charlie' },
+      ],
+      personalConversations: [
+        {
+          name: 'Alice',
+          lastMessage: 'Salut, comment ça va ?',
+          messages: [
+            { text: 'Salut, comment ça va ?', isMine: false },
+            { text: 'Ça va bien, merci !', isMine: true }
+          ]
+        },
+        {
+          name: 'damien',
+          lastMessage: 'On se voit ce soir ?',
+          messages: [
+            { text: 'On se voit ce soir ?', isMine: false },
+            { text: 'Oui, à 20h.', isMine: true }
+          ]
+        }
+      ],
+      groupConversations: [
+        {
+          name: 'groupe 1',
+          lastMessage: 'didididi',
+          messages: [
+            { text: 'loooo', isMine: false },
+            { text: 'hmh', isMine: true }
+          ]
+        },
+        {
+          name: 'grp2',
+          lastMessage: 'La réunion est reportée à demain.',
+          messages: [
+            { text: 'La réunion', isMine: false },
+            { text: 'D’accord', isMine: true }
+          ]
+        }
+      ],
+      selectedConversationIndex: 0,
+      selectedConversationType: 'personal',
+      newMessage: ''
     });
+  },
+  computed: {
+    filteredUsers() {
+      const query = this.searchQuery.toLowerCase();
+      return this.users.filter(user => user.name.toLowerCase().includes(query));
+    },
+    filteredPersonalConversations() {
+      return this.personalConversations.filter(conversation =>
+        conversation.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        conversation.lastMessage.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+    filteredGroupConversations() {
+      return this.groupConversations.filter(conversation =>
+        conversation.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        conversation.lastMessage.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+    selectedConversation() {
+      return this.selectedConversationType === 'personal'
+        ? this.personalConversations[this.selectedConversationIndex]
+        : this.groupConversations[this.selectedConversationIndex];
+    }
+  },
+  methods: {
+    selectConversation(index, type) {
+      this.selectedConversationIndex = index;
+      this.selectedConversationType = type;
+    },
+    async sendMessage() {
+      if (this.newMessage.trim() !== '') {
+        this.selectedConversation.messages.push({ text: this.newMessage, isMine: true });
 
     // Méthodes
     const loadGlobalMessages = async () => {
@@ -95,7 +231,7 @@ export default {
       } catch (error) {
         console.error('Erreur lors de la récupération des messages globaux:', error);
       }
-    };
+    }
 
     const loadConversationMessages = async (conversationId) => {
       const token = localStorage.getItem('token');
@@ -329,16 +465,26 @@ export default {
       createCommission,
       getReceivedNotifications
     };
-  },
-  mounted() {
+  }
+}
+},
+  mounted(){  
     this.getConversations();
     this.getUsers();
     this.getCommissions();
     this.getReceivedNotifications();
-  }
-};
-</script>
+  },
 
+    createUser() {
+      // Logique pour créer l'utilisateur (appel API, etc.)
+      this.showModal = false;
+    },
+    displayModal() {
+      this.showModal = true;
+    },
+    toggleSidebar() {
+      this.isSidebarOpen = !this.isSidebarOpen;
+    }}
+</script>
 <style scoped>
-/* Complétez si nécessaire */
 </style>
